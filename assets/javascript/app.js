3894439f00ea375a37177aaf6c1e0dff71e9af53
Lawan Petza <Lawan.Petza@gmail.com>
2018-06-14 10:46:33 -0400

    modified js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,120 +1,171 @@
 $(document).ready(function () {
     var topics = ["Messi", "Neymar", "Ronaldo"]
 
-    // function displayPlayer() {
-    //     console.log("Hello World")
-
-    //     for (var i = 0; i < topics.length; i++) {
-    //         var button = $("<button>")
-    //         button.addClass("gifButton")
-    //         button.attr("data-name", topics[i])
-    //         button.append(topics[i])
-    //         $("#buttonsView").prepend(button);
-    //     }
-    // }
-    // displayPlayer()
-
-    function renderButtons() {
-        $("#buttons-view").empty();
+    function displayPlayer() {
+        console.log("Hello World")
 
         for (var i = 0; i < topics.length; i++) {
-            var newButton = $("<button>");
-            newButton.addClass("gifButton")
-            newButton.attr("data-name", topics[i]);
-            newButton.text(topics[i]);
-
-            $("#buttons-view").append(newButton);
+            var button = $("<button>")
+            button.addClass("gifButton")
+            button.attr("data-name", topics[i])
+            button.append(topics[i])
+            $("#buttons-view").prepend(button);
         }
-
     }
-    renderButtons();
+    displayPlayer()
 
-    $("#add-player").on("click", function () {
-        event.preventDefault();
-        var player = $("#player-input").val().trim();
-        topics.push(player);
-        renderButtons();
-        // return false;
-    });
-
-
-    $("#buttons-view").on("click", ".gifButton", function () {
-        // console.log("click")
-
-        var soccerPlayer = $(this).attr("data-name")
-        console.log(soccerPlayer)
-
+     // Event listener for all button elements
+     $("button").on("click", function() {
+        // In this case, the "this" keyword refers to the button that was clicked
+        var soccerPlayer = $(this).attr("data-name");
+  
+        // Constructing a URL to search Giphy for the name of the person who said the quote
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            soccerPlayer + "&api_key=IjO0LqOOvP8SDiSxnFR1MoEjCH2pB108&limit=10";
-        console.log(queryURL);
-
+          soccerPlayer + "&api_key=dc6zaTOxFJmzC&limit=10";
+  
         // Performing our AJAX GET request
-        $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
-
-            // After the data comes back from the API
-
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          // After the data comes back from the API
+          .then(function(response) {
             // Storing an array of results in the results variable
             var results = response.data;
-            console.log(results);
-
+  
             // Looping over every result item
             for (var i = 0; i < results.length; i++) {
-                // Only taking action if the photo has an appropriate rating
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    // Creating a div with the class "item"
-                    // var gifDiv = $("<div class='item'>");
-                    // Storing the result item's rating
-                    var rating = results[i].rating;
-                    // Creating a paragraph tag with the result item's rating
-                    var ratingText = $("<p>").text("Rating: " + rating);
-
-                    // Creating an image tag
-
-                    var image = $("<image>")
-                    var gifImage = results[i].images.fixed_height.url;
-                    var stillImage = results[i].images.fixed_height_still.url;
-
-                    // Giving the image tag an src attribute of a proprty pulled off the
-                    // result item
-                   
-                    image.attr("src", stillImage);
-                    image.attr("alt", "gif");
-                    image.attr("data-state", "still");
-                    image.attr("data-still", stillImage);
-                    image.attr("data-animate", gifImage);
-                    // $('.image').html('<img class="img-thumbnail" src="' + stillImage + ' "data-state="still"' + '>');
-                    // $('.image').html('<img class="img-thumbnail" src="' + gifImage + ' "data-state="still"' + '>');
-        
-
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#players-view").prepend(image, ratingText);
-                    // checkState();
-                }
+  
+              // Only taking action if the photo has an appropriate rating
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                // Creating a div with the class "item"
+                var gifDiv = $("<div class='item'>");
+  
+                // Storing the result item's rating
+                var rating = results[i].rating;
+  
+                // Creating a paragraph tag with the result item's rating
+                var ratingText = $("<p>").text("Rating: " + rating);
+  
+                // Creating an image tag
+                var personImage = $("<img>");
+  
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                personImage.attr("src", results[i].images.fixed_height.url);
+  
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                gifDiv.append(ratingText);
+                gifDiv.append(personImage);
+  
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#players-view").prepend(gifDiv);
+              }
             }
-        });
-    });
-    // $(".img-thumbnail").on({
-    //     'click': function() {
-    //         var src = ($(this).attr('src') === stillImage) ?
-    //             stillImage :
-    //             gifImage;
-    //         $(this).attr('src', src);
-    //         console.log(img-thumbnail);
+          });
+      });
 
+    // function renderButtons() {
+    //     $("#buttons-view").empty();
+
+    //     for (var i = 0; i < topics.length; i++) {
+    //         var newButton = $("<button>");
+    //         newButton.addClass("gifButton")
+    //         newButton.attr("data-name", topics[i]);
+    //         newButton.text(topics[i]);
+
+    //         $("#buttons-view").append(newButton);
     //     }
+
+    // }
+    // renderButtons();
+
+    // $("#add-player").on("click", function () {
+    //     event.preventDefault();
+    //     var player = $("#player-input").val().trim();
+    //     topics.push(player);
+    //     renderButtons();
+    //     // return false;
     // });
 
 
-    $(".img").on("click", function () {
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    });
+    // $("#buttons-view").on("click", ".gifButton", function () {
+    //     // console.log("click")
+
+    //     var soccerPlayer = $(this).attr("data-name")
+    //     console.log(soccerPlayer)
+
+    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    //         soccerPlayer + "&api_key=IjO0LqOOvP8SDiSxnFR1MoEjCH2pB108&limit=10";
+    //     console.log(queryURL);
+
+    //     // Performing our AJAX GET request
+    //     $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
+
+    //         // After the data comes back from the API
+
+    //         // Storing an array of results in the results variable
+    //         var results = response.data;
+    //         console.log(results);
+
+    //         // Looping over every result item
+    //         for (var i = 0; i < results.length; i++) {
+    //             // Only taking action if the photo has an appropriate rating
+    //             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+    //                 // Creating a div with the class "item"
+    //                 // var gifDiv = $("<div class='item'>");
+    //                 // Storing the result item's rating
+    //                 var rating = results[i].rating;
+    //                 // Creating a paragraph tag with the result item's rating
+    //                 var ratingText = $("<p>").text("Rating: " + rating);
+
+    //                 // Creating an image tag
+
+    //                 var image = $("<image>")
+    //                 var gifImage = results[i].images.fixed_height.url;
+    //                 var stillImage = results[i].images.fixed_height_still.url;
+
+    //                 // Giving the image tag an src attribute of a proprty pulled off the
+    //                 // result item
+                   
+    //                 image.attr("src", stillImage);
+    //                 image.attr("alt", "gif");
+    //                 image.attr("data-state", "still");
+    //                 image.attr("data-still", stillImage);
+    //                 image.attr("data-animate", gifImage);
+    //                 // $('.image').html('<img class="img-thumbnail" src="' + stillImage + ' "data-state="still"' + '>');
+    //                 // $('.image').html('<img class="img-thumbnail" src="' + gifImage + ' "data-state="still"' + '>');
+        
+
+    //                 // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+    //                 $("#players-view").prepend(image, ratingText);
+    //                 // checkState();
+    //             }
+    //         }
+    //     });
+    // });
+    // // $(".img-thumbnail").on({
+    // //     'click': function() {
+    // //         var src = ($(this).attr('src') === stillImage) ?
+    // //             stillImage :
+    // //             gifImage;
+    // //         $(this).attr('src', src);
+    // //         console.log(img-thumbnail);
+
+    // //     }
+    // // });
+
+
+    // $(".img").on("click", function () {
+    //     var state = $(this).attr("data-state");
+    //     if (state === "still") {
+    //         $(this).attr("src", $(this).attr("data-animate"));
+    //         $(this).attr("data-state", "animate");
+    //     } else {
+    //         $(this).attr("src", $(this).attr("data-still"));
+    //         $(this).attr("data-state", "still");
+    //     }
+    // });
 
 
 
