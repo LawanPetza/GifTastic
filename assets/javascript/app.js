870c310b829043ea75a556970c443f11b02f79f0
Lawan Petza <Lawan.Petza@gmail.com>
2018-08-27 21:10:38 -0400

    modified app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     var players = ["Messi", "Neymar", "Ronaldo", "Harry Kane", "Manuel Neuer", "Stuart Franklin", "Toni Kroos"]
 
+    //function to make buttons and add page
     function makeButtons() {
-        console.log(makeButtons)
 
         $("#buttons-view").empty();
+
         // loops through the players array
         for (var i = 0; i < players.length; i++) {
             var button = $("<button>")
@@ -14,9 +15,14 @@ $(document).ready(function () {
             $("#buttons-view").append(button);
         }
     }
-    makeButtons()
+    // makeButtons()
+
     // Event listener for all button elements
-    $(document).on("click",".soccerBtn", function () {
+    $(document).on("click", ".soccerBtn", function () {
+        $("#players-view").empty();
+        $("#soccerBtn").removeClass("active");
+        $(this).addClass("active")
+
         // In this case, the "this" keyword refers to the button that was clicked
         var soccerPlayer = $(this).attr("data-name");
 
@@ -31,19 +37,18 @@ $(document).ready(function () {
         })
             // After the data comes back from the API
             .then(function (response) {
-                console.log(response.data);
-                
+
                 // Storing an array of results in the results variable
                 var results = response.data;
-                console.log(results)
 
                 // Looping over every result item
                 for (var i = 0; i < results.length; i++) {
 
                     // Only taking action if the photo has an appropriate rating
                     if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
                         // Creating a div with the class "item"
-                        var gifDiv = $("<div class='item'>");
+                        var gifDiv = $("<div class='player-item'>");
 
                         // Storing the result item's rating
                         var rating = results[i].rating;
@@ -51,25 +56,17 @@ $(document).ready(function () {
                         // Creating a paragraph tag with the result item's rating
                         var ratingText = $("<p>").text("Rating: " + rating);
 
-                        // Creating an image tag
-                        var personImage = $("<img class = 'soccerPlayer'>");
-
-
-                        // Giving the image tag an src attribute of a proprty pulled off the
-                        // result item
-                        // personImage.attr("src", results[i].images.fixed_height_still.url);
-
                         var gifImage = results[i].images.fixed_height.url;
                         var stillImage = results[i].images.fixed_height_still.url;
 
-
+                        // Creating an image tag
+                        var personImage = $("<img>");
                         personImage.attr("src", stillImage);
-                        personImage.attr("alt", "gif");
-                        personImage.attr("data-state", "still");
                         personImage.attr("data-still", stillImage);
                         personImage.attr("data-animate", gifImage);
-
-
+                        personImage.attr("data-state", "still");
+                        personImage.addClass("soccerPlayer");
+                        
                         // Appending the paragraph and personImage we created to the "gifDiv" div we created
                         gifDiv.append(ratingText);
                         gifDiv.append(personImage);
@@ -80,12 +77,11 @@ $(document).ready(function () {
                 }
             });
 
-        $(document).on("click", ".soccerPlayer", pauseGifPlayer);
+        $(document).on("click", ".soccerPlayer", function() {
 
-        //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
-        function pauseGifPlayer() {
-            
+            //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
             var state = $(this).attr("data-state");
+
             if (state === "still") {
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("data-state", "animate");
@@ -93,29 +89,26 @@ $(document).ready(function () {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
             }
-        }
+
+        });
 
 
     });
-    //add-player button event
-    $("#add-player").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var soccerplayer = $("#player-input").val().trim();
-        console.log(soccerplayer)
 
-        // Adding movie from the textbox to our array
-        players.push(soccerplayer);
+    //add-player button event
+    $("#add-player").on("click", function (event) {
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var newPlayer = $("#player-input").val().trim();
+
+        // Adding player from the textbox to our array
+        players.push(newPlayer);
 
         makeButtons();
-        return false;
 
-        // Calling renderButtons which handles the processing of our movie array
-        // renderButtons();
-      });
+    });
 
-    //   renderButtons();
-
-
+    makeButtons();
 
 });
